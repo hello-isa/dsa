d9978a6fe775d098be022497cb91cb884fdb2f42
hello-isa <isabeljoyadriatico@gmail.com>
2024-02-10 15:19:26 +0800

    Rename files

---
+++ 2-ADT List Array/2-adt-array-v1.c
@@ -0,0 +1,7 @@
+#define MAX 5
+
+typedef struct
+{
+    char arr[MAX]; // 5 bytes
+    int count;     // 4 bytes
+} LIST;            // 9 bytes
---
+++ 2-ADT List Array/3-adt-array-v2.c
@@ -0,0 +1,7 @@
+#define MAX 5
+
+typedef struct node // Structure data type: 9 bytes
+{
+    char arr[MAX];
+    int count;
+} *LIST; // Pointer to structure: 8 bytes
