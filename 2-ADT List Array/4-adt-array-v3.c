23aa489017461debef2fa43a46bb3ddf65c63aef
hello-isa <isabeljoyadriatico@gmail.com>
2024-02-11 14:22:00 +0800

    Create 4-adt-array-v3.c and 5-adt-array-v4.c, add comment in 3-adt-array-v2.c

---
+++ 2-ADT List Array/4-adt-array-v3.c
@@ -0,0 +1,7 @@
+#define MAX 5
+
+typedef struct
+{
+    char *elemPtr; // 8 bytes
+    int count;     // 4 bytes
+} LIST;            // 12 bytes
--- 2-ADT List Array/5-adt-array-v4.c
+++ 2-ADT List Array/5-adt-array-v4.c
@@ -1,7 +1,7 @@
 #define MAX 5
 
-typedef struct node // Structure data type: 9 bytes
+typedef struct node // Structure data type: 12 bytes
 {
-    char arr[MAX]; // 5 bytes
+    char *elemPtr; // 8 bytes
     int count;     // 4 bytes
 } *LIST;           // Pointer to structure: 8 bytes
