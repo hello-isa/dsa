c65b6e5f6669a7a0c5e3b3aeeeb5494a37c5c6b1
hello-isa <isabeljoyadriatico@gmail.com>
2024-02-11 14:59:24 +0800

    Update 2-adt-array-v1.c

--- 2-ADT List Array/2-adt-array-v1.c
+++ 2-ADT List Array/2-adt-array-v1.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #define MAX 5
 
 typedef struct
@@ -5,3 +7,30 @@ typedef struct
     char arr[MAX]; // 5 bytes
     int count;     // 4 bytes
 } LIST;            // 9 bytes
+
+void initializeList(LIST *L);
+
+int main()
+{
+    LIST Arr;
+
+    initializeList(&Arr);
+
+    return 0;
+}
+
+void initializeList(LIST *L)
+{
+    L = (LIST *)malloc(sizeof(LIST));
+
+    if (L != NULL)
+    {
+        L->count = 0;
+    }
+    else
+    {
+        /* If malloc fails, it will return a NULL pointer
+        instead of a valid memory address.*/
+        printf("Memory allocation failed.\n");
+    }
+}
