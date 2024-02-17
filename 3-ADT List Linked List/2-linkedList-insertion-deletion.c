6eeecdfd564c2ae937408a0ca2d8073c2024d7b8
hello-isa <isabeljoyadriatico@gmail.com>
2024-02-17 16:05:48 +0800

    Rename file

--- 3-ADT List Linked List/2-linkedList-insertion-deletion.c
+++ 3-ADT List Linked List/2-linkedList-insertion-deletion.c
@@ -10,12 +10,14 @@ typedef struct node
 void displayList(LIST L);
 
 // Insertion
-void insertFirst(LIST *L, char X);
-void insertLast(LIST *L, char X);
-void insertPos(LIST *L, char X, int position);
+void insertFirst(LIST *L, char X);             // PN
+void insertLast(LIST *L, char X);              // PPN
+void insertPos(LIST *L, char X, int position); // PPN
 
 // Deletion
-void deleteFirst(LIST *L);
+void deleteFirst(LIST *L); // PN
+void deleteLast(LIST *L);  // PPN
+void deletePos(LIST *L, int position);
 
 int main()
 {
@@ -29,21 +31,38 @@ int main()
 
     printf("\nInsert last:\n");
     insertLast(&LL, 'Z');
-    insertLast(&LL, 'Z');
+    insertLast(&LL, 'Y');
     insertLast(&LL, 'X');
     displayList(LL);
 
-    printf("\nInsert at position 1:\n");
-    insertPos(&LL, 'F', 1);
+    printf("\nInsert at position 0:\n");
+    insertPos(&LL, 'F', 0);
     displayList(LL);
 
-    printf("\nInsert at position 4:\n");
-    insertPos(&LL, 'H', 4);
+    printf("\nInsert at position 6:\n");
+    insertPos(&LL, 'H', 6);
     displayList(LL);
 
     printf("\nDelete first:\n");
     deleteFirst(&LL);
     deleteFirst(&LL);
+    displayList(LL);
+
+    printf("\nDelete last:\n");
+    deleteLast(&LL);
+    deleteLast(&LL);
+    displayList(LL);
+
+    printf("\nDelete at position 1:\n");
+    deletePos(&LL, 1);
+    displayList(LL);
+
+    printf("\nDelete at position 2:\n");
+    deletePos(&LL, 2);
+    displayList(LL);
+
+    printf("\nDelete at position 0:\n");
+    deletePos(&LL, 0);
     displayList(LL);
 
     return 0;
@@ -146,8 +165,35 @@ void deleteFirst(LIST *L)
     else
     {
         // Scenario 2: List is not empty
-        LIST temp = *L;
+        LIST temp = *L;  // Pointer to node
         *L = temp->link; // L is a pointer to a pointer to a node hence you need to dereference it
         free(temp);
     }
+}
+
+void deleteLast(LIST *L)
+{
+    LIST *trav; // Pointer to pointer to node
+    for (trav = L; (*trav)->link != NULL; trav = &(*trav)->link)
+    {
+    }
+    LIST temp;
+    temp = (*trav)->link;
+    *trav = temp;
+    free(temp);
+}
+
+void deletePos(LIST *L, int position)
+{
+    LIST *trav; // Pointer to pointer to node
+    int count = 0;
+    for (trav = L; (*trav)->link != NULL && count < position; trav = &(*trav)->link)
+    {
+        count++;
+    }
+
+    LIST temp;
+    temp = (*trav)->link;
+    (*trav) = temp;
+    free(temp);
 }
