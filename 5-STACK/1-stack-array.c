897f218c08639f7ad3b989f1f9ca6c0b3f8e8368
hello-isa <isabeljoyadriatico@gmail.com>
2024-02-18 16:51:28 +0800

    Update 1-stack-array.c

--- 5-STACK/1-stack-array.c
+++ 5-STACK/1-stack-array.c
@@ -1,4 +1,6 @@
+// Array list version 2 implementation
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX 5
 
 typedef struct node
@@ -7,27 +9,73 @@ typedef struct node
     int top;
 } *STACK;
 
+typedef enum
+{
+    F, // False
+    T  // True
+} boolean;
+
 void initializeStack(STACK *S);
 void displayStack(STACK S);
 void push(STACK *S, char elem);
+char pop(STACK *S);
+char peek(STACK S);
+boolean isEmpty(STACK S);
+boolean isFull(STACK S);
 
 int main()
 {
     STACK SS;
 
     initializeStack(&SS);
+
     push(&SS, 'a');
     push(&SS, 'b');
     push(&SS, 'c');
     push(&SS, 'd');
+    push(&SS, 'e');
     displayStack(SS);
+
+    char popped = pop(&SS);
+    printf("Popped element: %c\n", popped);
+    displayStack(SS);
+
+    char peeked = peek(SS);
+    printf("Peeked element: %c\n", peeked);
+    displayStack(SS);
+
+    if (isEmpty(SS) == T)
+    {
+        printf("Stack is EMPTY.\n");
+    }
+    else
+    {
+        printf("Stack is NOT empty.\n");
+    }
+
+    if (isFull(SS) == T)
+    {
+        printf("Stack is FULL.\n");
+    }
+    else
+    {
+        printf("Stack is NOT full.\n");
+    }
 
     return 0;
 }
 
 void initializeStack(STACK *S)
 {
-    (*S)->top = -1; // Empty stack
+    (*S) = (STACK)malloc(sizeof(struct node));
+    if (*S != NULL)
+    {
+        (*S)->top = -1; // Empty stack
+    }
+    else
+    {
+        printf("Memory allocation failed.\n");
+    }
 }
 
 void displayStack(STACK S)
@@ -45,6 +93,29 @@ void displayStack(STACK S)
 
 void push(STACK *S, char elem)
 {
-    (*S)->arr[(*S)->top + 1] = elem;
     (*S)->top += 1;
+    (*S)->arr[(*S)->top] = elem;
+}
+
+char pop(STACK *S)
+{
+    char elem = (*S)->arr[(*S)->top];
+    (*S)->top -= 1;
+
+    return elem;
+}
+
+char peek(STACK S)
+{
+    return S->arr[S->top];
+}
+
+boolean isEmpty(STACK S)
+{
+    return S->top == -1 ? T : F;
+}
+
+boolean isFull(STACK S)
+{
+    return S->top == (MAX - 1) ? T : F;
 }
