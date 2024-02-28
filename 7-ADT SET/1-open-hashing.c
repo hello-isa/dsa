d0ea3078ac45d54c01bc599418630700397075e7
hello-isa <isabeljoyadriatico@gmail.com>
2024-02-28 22:02:14 +0800

    Update 1-open-hashing.c

--- 7-ADT SET/1-open-hashing.c
+++ 7-ADT SET/1-open-hashing.c
@@ -23,6 +23,7 @@ void displayDictionary(Dictionary D);     // Display the elements of the Diction
 int hash(int elem);                       // Returns the ones value to locate which index (i.e. 0 to 9) to insert the new element
 Boolean isMember(Dictionary D, int elem); // Returns TRUE if the element is a member; else returns FALSE
 void insert(Dictionary D, int elem);      // Insert an element into the Dictionary
+void delete(Dictionary D, int elem);      // Delete an element from the Dictionary
 
 int main()
 {
@@ -41,7 +42,13 @@ int main()
     insert(A, 108);
     insert(A, 28);
     insert(A, 48);
-    //    insert(A, 20);
+    // insert(A, 20); // Existing element
+    displayDictionary(A);
+
+    delete (A, 20);
+    delete (A, 33);
+    delete (A, 28);
+    // delete (A, 11); // Non-existent element
     displayDictionary(A);
 
     return 0;
@@ -104,7 +111,7 @@ void insert(Dictionary D, int elem)
     }
     else
     {
-        // Element does not exist. Insertion is allowed.
+        // Element does not exist hence insertion is allowed
         // Elements must be sorted
         // Implement PPN since insertion
 
@@ -127,7 +134,30 @@ void insert(Dictionary D, int elem)
         {
         }
         temp = *trav;
-        newNode->link = temp;
         *trav = newNode;
+        newNode->link = temp;
+    }
+}
+
+void delete(Dictionary D, int elem)
+{
+    if ((isMember(D, elem)) == False)
+    {
+        printf("Element does NOT exist. Deletion is NOT allowed.\n");
+    }
+    else
+    {
+        // Element exist in the list hence deletion is allowed
+        // Implement PPN since deletion
+        SET *trav, temp;
+
+        int pos = hash(elem);
+
+        for (trav = &(D[pos]); *trav != NULL && (*trav)->elem != elem; trav = &(*trav)->link)
+        {
+        }
+        temp = *trav;
+        *trav = temp->link;
+        free(temp);
     }
 }
