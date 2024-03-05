6a89fb877435c7111583d043678cce129f47941d
hello-isa <isabeljoyadriatico@gmail.com>
2024-03-05 22:11:09 +0800

    Update 2-closed-hashing.c

--- 7-ADT SET/2-closed-hashing.c
+++ 7-ADT SET/2-closed-hashing.c
@@ -18,8 +18,8 @@ void initDictionary(Dictionary D);         // Initialize Dictionary to be EMPTY
 void displayDictionary(Dictionary D);      // Display the content of the Dictionary
 int hash(char elem);                       // Returns the arbitrary hash values
 void insert(Dictionary D, char elem);      // Insert an element into the Dictionary
-void delete(Dictionary D, char elem);      // Delete an element from the Dictionary
 Boolean isMember(Dictionary D, char elem); // Returns TRUE if an element is a member of the Dictionary; else FALSE
+void delete(Dictionary D, char elem);      // Delete an element from the Dictionary
 
 int main()
 {
@@ -44,11 +44,11 @@ int main()
     displayDictionary(A);
 
     delete (A, 'a');
-    delete (A, 'c'); // Hash value is 2 but the dictionary contains 'b' so it needs to look for 'c'
+    delete (A, 'c');
     delete (A, 'g');
     delete (A, 'h');
-    delete (A, 'a'); // Hash value is 0 but is already deleted so it needs to look for 'a'
     delete (A, 'a');
+    delete (A, 'a'); // Non-existent
     displayDictionary(A);
 
     return 0;
@@ -110,23 +110,35 @@ void insert(Dictionary D, char elem)
     }
 }
 
-void delete(Dictionary D, char elem)
+Boolean isMember(Dictionary D, char elem)
 {
     int n, count;
+    Boolean member = False;
 
     for (n = hash(elem), count = 0; count < MAX && D[n] != elem; n = (n + 1) % MAX, count++)
     {
     }
     if (D[n] == elem)
     {
+        member = True;
+    }
+
+    return member;
+}
+
+void delete(Dictionary D, char elem)
+{
+    int n, count;
+
+    for (n = hash(elem), count = 0; D[n] != elem && count < MAX; n = (n + 1) % MAX, count++)
+    {
+    }
+    if (isMember(&D[n], elem) == True)
+    {
         D[n] = DELETED;
     }
     else
     {
-        printf("Deletion is NOT allowed. '%c' may have been already or is not in the Dictionary.\n", elem);
+        printf("Deletion of '%c' is NOT allowed. Element is not found in the Dictionary.\n", elem);
     }
-}
-
-Boolean isMember(Dictionary D, char elem)
-{
 }
